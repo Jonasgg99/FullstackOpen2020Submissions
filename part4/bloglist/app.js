e2168a88031a437be8713af44573cebf314d96a7
Jonas <jonaseg@gmail.com>
2021-02-01 16:25:48 +0100

    stable

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,11 +4,13 @@ const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const blogsRouter = require("./controllers/blogs")
+const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
 
 morgan.token("text", function (req, res) { return JSON.stringify(req.body) });
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :text :date[web]"));
-console.log("Connecting to MongoDB...");
+logger.info("Connecting to MongoDB...");
 //Connecting to server
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
 
@@ -16,5 +18,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
---
+++ part4/bloglist/utils/middleware.js
@@ -0,0 +1,16 @@
+const logger = require("../utils/logger");
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: "unknown endpoint"});
+};
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message);
+  
+  next(error);
+};
+
+module.exports = {
+  unknownEndpoint,
+  errorHandler,
+};
