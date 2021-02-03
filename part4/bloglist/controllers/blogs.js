48c038e8ee3981c17551892520f490d75f4d3c2c
Jonas <jonaseg@gmail.com>
2021-02-03 22:03:13 +0100

    4b done

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -19,4 +19,20 @@ blogsRouter.post("/", async (request, response) => {
   response.json(saved);
 });
 
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put("/:id", async (request, response) => {
+  const blogToUpdate = await Blog.findById(request.params.id);
+
+  const updatedBlog = {
+    likes: request.body.likes
+  };
+  
+  await Blog.findByIdAndUpdate(blogToUpdate.id, updatedBlog, {new:true});
+  response.json(`${blogToUpdate.author}'s blog ${blogToUpdate.title} updated to ${request.body.likes} likes`);
+});
+
 module.exports = blogsRouter;
