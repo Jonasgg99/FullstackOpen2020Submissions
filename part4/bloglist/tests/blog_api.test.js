153cd02ae8c008f490613b37bbbc3c388f9e67b6
Jonas <jonaseg@gmail.com>
2021-02-06 19:20:20 +0100

    part 4-4 complete

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const api = supertest(app);
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 const initialBlogs = [
   {
@@ -19,13 +22,27 @@ const initialBlogs = [
     likes:2,
   }
 ];
-
+let token;
 beforeEach(async () => {
   await Blog.deleteMany({});
   let blogObject = new Blog(initialBlogs[0]);
   await blogObject.save();
   blogObject = new Blog(initialBlogs[1]);
   await blogObject.save();
+
+  await User.deleteMany({});
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash('sekret', saltRounds);
+  const user = new User({ username: 'sampleUsername', passwordHash });
+
+  await user.save();
+
+  const userForToken = {
+    username: user.username,
+    id: user.id,
+  };
+
+  token = jwt.sign(userForToken, process.env.SECRET)
 });
 
 test("blogs are returned as json", async () => {
@@ -48,54 +65,57 @@ test('id is defined', async () => {
 });
 
 test('added blogs without likes default to 0', async () => {
-  const newBlog = {
-    title: 'blog without likes',
-    url: 'testurl',
-    author: "testMan",
+  const newBlogPost = {
+    title: 'Test Title 1',
+    author: 'Test Author 1',
+    url: 'Test URL 1'
   };
 
   await api
     .post('/api/blogs')
-    .send(newBlog)
+    .set('Authorization', `Bearer ${token}`)
+    .send(newBlogPost)
     .expect(200)
     .expect((response) => {
-      response.body.likes === 0;
+      response.body.likes === 0
     });
     
 });
 
 test('a blog can be added', async () => {
-  const newBlog = {
-    title: 'async/await simplifies making async calls',
-    url: 'testurl',
-    author: "testMan",
+  const newBlogPost = {
+    title: 'Test Title 1',
+    author: 'Test Author 1',
+    url: 'Test URL 1',
+    likes: 12,
   };
 
   await api
     .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-
+    .set('Authorization', `Bearer ${token}`)
+    .send(newBlogPost)
+    .expect(200);
+    
   const response = await api.get('/api/blogs');
 
   const titles = response.body.map(r => r.title);
 
   expect(response.body).toHaveLength(initialBlogs.length + 1);
   expect(titles).toContain(
-    'async/await simplifies making async calls'
+    'Test Title 1'
   );
 });
 
 test('missing title and url', async () => {
-  const newBlog = new Blog({
-    author: "testauthor",
-    likes: 5
-  });
+  const newBlogPost = {
+    author: 'Test Author 1',
+    likes: 12,
+  };
 
   await api
     .post('/api/blogs')
-    .send(newBlog)
+    .set('Authorization', `Bearer ${token}`)
+    .send(newBlogPost)
     .expect(400);
 });
 
