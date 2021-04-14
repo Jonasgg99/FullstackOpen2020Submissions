71daac5c3ca0dc6b1815085ca7a784437e9285bd
Jonas <jonaseg@gmail.com>
2021-04-14 21:10:37 +0200

    8.10

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -4,26 +4,8 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import { gql, useQuery } from '@apollo/client';
+import { ALL_BOOKS, ALL_AUTHORS } from './queries'
 
-const ALL_AUTHORS = gql`
-query {
-  allAuthors  {
-    name
-    born
-    bookCount
-  }
-}
-`
-
-const ALL_BOOKS = gql`
-query {
-  allBooks  {
-    title
-    author
-    published
-  }
-}
-`
 
 const App = () => {
   const [page, setPage] = useState('authors')
